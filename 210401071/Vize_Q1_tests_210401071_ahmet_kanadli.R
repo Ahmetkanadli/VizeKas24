763473a14665b15840e9f65315f8bcaa7aa0e7e9
Ahmetkanadli <kanadliahmet5@gmail.com>
2023-11-24 16:12:31 +0300

    Özellik 2, testleri ile tamamlandı

--- 210401071/Vize_Q1_tests_210401071_ahmet_kanadli.R
+++ 210401071/Vize_Q1_tests_210401071_ahmet_kanadli.R
@@ -14,8 +14,13 @@ relative_path <- file.path(current_dir, "Vize_Q1_210401071_ahmet_kanadli.R")
 
 source(relative_path)
 
-
+#1.1
 
 test_that("Global Workspace’de spotify_token adlı bir değişken olmalı", {
   expect_true(exists("spotify_token"))
+})
+
+# Test 1.2: spotify_token adlı değişkenin tipi “function” olmalı.
+test_that("spotify_token adlı değişkenin tipi 'function'", {
+  expect_true(is.function(spotify_token))
 })
